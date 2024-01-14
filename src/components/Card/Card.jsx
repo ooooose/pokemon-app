803f81aa00ae7c5b3313dd231a40f6c2952a0672
ooooose <yuuki.oose1990@gmail.com>
2024-01-14 19:49:21 +0900

    Add #6 詳細情報の表示

--- src/components/Card/Card.jsx
+++ src/components/Card/Card.jsx
@@ -2,8 +2,31 @@ import React from "react";
 
 const Card = ({pokemon}) => {
   return (
-    <div>
-      Pokemon
+    <div className="card">
+      <div className="cardImg">
+        <img src={pokemon.sprites.front_default} alt={pokemon.name} />
+      </div>
+      <h3>{pokemon.name}</h3>
+      <div className="cardTypes">
+        <div>タイプ</div>
+        {pokemon.types.map((type, i) => {
+          return (
+            <div key={`${pokemon.name}-${i}`}>
+              <span className="typeName">{type.type.name}</span>
+            </div>
+        )})}
+      </div>
+      <div className="cardInfo">
+        <div className="cardData">
+          <p className="title">重さ：{pokemon.weight}</p>
+        </div>
+        <div className="cardData">
+          <p className="title">高さ：{pokemon.height}</p>
+        </div>
+        <div className="cardData">
+          <p className="title">アビリティ：{pokemon.abilities[0].ability.name}</p>
+        </div>
+      </div>
     </div>
   )
 }
